3bc52a3a5dba9deabd8810759b9ec7f100e1f8e4
Melody Universe <melody.universe.2021@gmail.com>
2023-06-29 10:00:57 -0700

    instantiated test projects

---
+++ test/HighLevel.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace HighLevel.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/LowLevel.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace LowLevel.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}

65f98b43c3ba52f13dc37afbe6ee14cde3198afc
Melody Universe <melody@computost.com>
2024-03-21 22:02:24 -0700

    feat: created bump-version script

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,10 +1,28 @@
-version=$(
-  echo $(
-    git describe --tags $(
-      git rev-list --tags --max-count=1
+bump_type=$1
+if [ "$bump_type" = 'alpha' ]
+then
+  version=$(
+    echo $(
+      git describe --tags $(
+        git rev-list --tags --max-count=1
+      )
+    ) | sed -e 's/^v//g'
+  )
+
+  if [[ "$version" =~ beta\.[0-9]+$ ]]
+  then
+    version=$(
+      echo "$version" \
+      | sed -r 's/-beta\.[0-9]+//g'
     )
-  ) | sed -e 's/^v//g'
-)
-sed -i -e "s/<Version>[^<]*/<Version>$version/g" ./Directory.Build.props 
-git add .
-git commit -m "chore: sync version to tag"
+  fi
+
+  sed -i -e "s/<Version>[^<]*/<Version>$version/g" ./Directory.Build.props
+  git add .
+  git commit -m 'chore: pre-versionize'
+  dotnet versionize --pre-release alpha --find-release-commit-via-message
+elif [ "$bump_type" = "beta" ]
+  dotnet versionize --pre-release beta
+else
+  dotnet versionise --aggregate-pre-releases
+fi

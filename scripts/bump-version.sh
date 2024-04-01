e0e12d91be0fd6d0a6a68d1269b2eb1952eeaea0
Melody Universe <melody@computost.com>
2024-03-31 22:42:00 -0700

    fix: created version bump script

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,38 +1,146 @@
+set_build_props_version() {
+  version=$1
+
+  sed -i "s/<Version>[^<]*/<Version>$(
+      echo $version |
+        sed 's/^v//' |
+        sed -r 's/-beta\.[0-9]+$//'
+    )/g" \
+    ./Directory.Build.props
+}
+
+get_last_general_release() {
+  for tag in $(git tag --sort=-refname)
+  do
+    if [[ $tag =~ ^v([0-9]+\.){2}[0-9]+$ ]]
+    then
+      echo $tag
+      return 0
+    fi
+  done
+}
+
+get_version_part() {
+  version="$1"
+  part="$2"
+
+  case $part in
+    major)
+      echo $version |
+        sed -rn 's/^v?([[:digit:]]+).*$/\1/p'
+      ;;
+
+    minor)
+      echo $version |
+        sed -rn 's/^v?[[:digit:]]+\.([[:digit:]]+).*$/\1/p'
+      ;;
+
+    patch)
+      echo $version |
+        sed -rn 's/^v?([[:digit:]]+\.){2}([[:digit:]]+).*$/\2/p'
+      ;;
+
+    prerelease)
+      echo $version |
+        sed -rn 's/^v?([[:digit:]]+\.){2}[[:digit:]]+-(.+)$/\2/p'
+      ;;
+
+    *)
+      echo "unrecognized part: $part" 1>&2
+      return 1
+  esac
+}
+
+get_version_part_diff() {
+  version_1=$1
+  version_2=$2
+  part=$3
+  
+  echo "$((
+    $(get_version_part $version_1 $part)
+    - $(get_version_part $version_2 $part)))"
+}
+
+get_next_patch_version() {
+  version=$1
+
+  current_directory=$(pwd)
+  temp_repo="${RUNNER_TEMP:-.}/temp_repo"
+  mkdir $temp_repo
+  cp ./Directory.Build.props $temp_repo
+  cd $temp_repo
+  git init --quiet
+  git config user.name temp
+  git config user.email temp
+  set_build_props_version $version
+  git add .
+  git commit -m 'chore: init' --quiet
+  dotnet versionize --silent --skip-tag
+  git commit --allow-empty -m 'fix: bump' --quiet
+  dotnet versionize \
+    --find-release-commit-via-message \
+    --pre-release alpha \
+    --silent \
+    --skip-tag
+  echo $(dotnet versionize inspect)
+  cd $current_directory
+  rm -rf $temp_repo
+}
+
+remove_pre_release() {
+  version=$1
+  echo $version | sed -rn 's/-[a-z]+\.[0-9]+//p'
+}
+
 bump_type=$1
 if [ "$bump_type" = 'alpha' ]
 then
-  echo "alpha bump"
-  version=$(
-    echo $(
-      git describe --tags $(
-        git rev-list --tags --max-count=1
-      )
-    ) | sed -e 's/^v//g'
+  current_version=$(
+    git describe --tags $(
+      git rev-list --tags --max-count=1
+    )
   )
 
-  if [[ "$version" =~ beta\.[0-9]+$ ]]
+  set_build_props_version $current_version
+  git add .
+  git commit --allow-empty -m 'chore: pre-versionize' --quiet
+  dotnet versionize \
+    --find-release-commit-via-message \
+    --pre-release alpha \
+    --silent \
+    --skip-tag
+  tentative_version=$(dotnet versionize inspect)
+  git reset --hard HEAD~2
+
+  last_general_release=$(get_last_general_release)
+  echo "tentative version: $tentative_version"
+  echo "last_general_release: $last_general_release"
+  echo "major diff: $(get_version_part_diff $tentative_version $last_general_release major)"
+  echo "minor diff: $(get_version_part_diff $tentative_version $last_general_release minor)"
+  if [ $(get_version_part_diff $tentative_version $last_general_release major) -gt 1 ] \
+    || ([ $(get_version_part_diff $tentative_version $last_general_release major) -eq 1 ] \
+      && [ $(get_version_part $tentative_version minor) -gt 0 ]) \
+    || ([ $(get_version_part_diff $tentative_version $last_general_release major) -eq 0 ] \
+      && [ $(get_version_part_diff $tentative_version $last_general_release minor) -gt 1 ])
   then
-    version=$(
-      echo "$version" \
-      | sed -r 's/-beta\.[0-9]+//g'
-    )
+    echo 'getting next patch version instead'
+    new_version=$(get_next_patch_version $current_version)
+  else
+    new_version=$tentative_version
   fi
 
-  sed -i -e "s/<Version>[^<]*/<Version>$version/g" ./Directory.Build.props
-  git add .
-  git commit -m 'chore: pre-versionize'
   dotnet versionize \
-    --pre-release alpha \
-    --find-release-commit-via-message
+    --exit-insignificant-commits \
+    --release-as $new_version
 elif [ "$bump_type" = "beta" ]
 then
-  echo "beta bump"
+  current_version=$(dotnet versionize inspect)
   dotnet versionize \
-    --pre-release beta \
-    --aggregate-pre-releases \
-    --find-release-commit-via-message
+    --release-as "$(remove_pre_release $current_version)-beta.0" \
+    --aggregate-pre-releases
 else
+  current_version=$(dotnet versionize inspect)
   dotnet versionize \
-    --aggregate-pre-releases \
-    --find-release-commit-via-message
+    --release-as $(remove_pre_release $current_version) \
+    --aggregate-pre-releases
 fi
